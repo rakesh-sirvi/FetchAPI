2809c02ab5c4460060f4f1619f69c26b9bc091e8
Rakesh Sirvi <rsirvi797@gmail.com>
2021-08-24 22:58:43 +0530

    Updates

---
+++ precache-manifest.a52b3011429075b51ff21d23522a92b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c380d6bc2055ca70c2d31a69edf733",
+    "url": "/FetchAPI/index.html"
+  },
+  {
+    "revision": "76b261f1f9e2f015dbe3",
+    "url": "/FetchAPI/static/css/2.67f6656e.chunk.css"
+  },
+  {
+    "revision": "2112c339c6231b003812",
+    "url": "/FetchAPI/static/css/main.99f66ed7.chunk.css"
+  },
+  {
+    "revision": "76b261f1f9e2f015dbe3",
+    "url": "/FetchAPI/static/js/2.1486e3f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FetchAPI/static/js/2.1486e3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2112c339c6231b003812",
+    "url": "/FetchAPI/static/js/main.3280e9b5.chunk.js"
+  },
+  {
+    "revision": "b165495e96b57f096956",
+    "url": "/FetchAPI/static/js/runtime-main.1b4d0504.js"
+  }
+]);
